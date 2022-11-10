ee2c5fe3ed1fbe848f79e362406481c6d8516b41
mateusz.konieczny@hotmail.com <Javovanie576^%#*>
2022-11-10 17:28:15 +0100

    -Add serviceTask Approach

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/mongo-db/index.html'));});
+app.use(requireHTTPS);
+app.use(express.static('./dist/wamu-list-manager'));
 
-  app.listen(process.env.PORT || 8080);
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/wamu-list-manager/'}),
+);
+
+app.listen(process.env.PORT || 8080);
